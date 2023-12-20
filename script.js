33655007717fdf7dd4298b2ad9735a13643e19f7
Mike Nguyen <mikenguyen@Mikes-Air.lan>
2023-12-19 22:29:07 -0500

    rewrote comments

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-// Array of special characters to be included in password
+// Special characters 
 let specialCharacters = [`@`,`%`,`+`,`\\`,`/`,`'`,`!`,`#`,`$`,`^`,`?`,`:`,`,`,`)`,`(`,`}`,`{`,`]`,`[`,`~`,`-`,`_`,`.`,];
 
-// Array of numeric characters to be included in password
+// Numeric characters 
 let numericCharacters = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
 
-// Array of lowercase characters to be included in password
+// Lowercase characters 
 let lowerCasedCharacters = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`,];
 
-// Array of uppercase characters to be included in password
+// Uppercase characters 
 let upperCasedCharacters = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`,];
 
 //Write password to the #password input
@@ -24,5 +24,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-console.log(specialCharacters);
