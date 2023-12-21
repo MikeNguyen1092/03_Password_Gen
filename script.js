1a87ceea341c3102e0568c28703aea3bb1effba1
Mike Nguyen <mikenguyen@Mikes-Air.lan>
2023-12-21 01:01:32 -0500

    reword comments,prompts, and alerts to sound better. Added recall generatePassword function if they want to restart or cancel

--- script.js
+++ script.js
@@ -1,61 +1,62 @@
-// Assignment Code, querySelector is used to select id=`generate` in HTML and assign it to generateBtn
+                // Assignment Code, querySelector is used to select id=`generate` in HTML and assign it to generateBtn
 let generateBtn = document.querySelector(`#generate`);
 
-// Special characters 
+                // Global variables 
+                // Special characters 
 let specialCharacters = [`@`,`%`,`+`,`\\`,`/`,`'`,`!`,`#`,`$`,`^`,`?`,`:`,`,`,`)`,`(`,`}`,`{`,`]`,`[`,`~`,`-`,`_`,`.`];
 
-// Numeric characters 
+                // Numeric characters 
 let numericCharacters = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
 
-// Lowercase characters 
+                // Lowercase characters 
 let lowerCasedCharacters = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
 
-// Uppercase characters 
+                // Uppercase characters 
 let upperCasedCharacters = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
 
-//Write password to the #password input on the HTML
+                // Function to write password to the #password input on the HTML
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-// function is called when button is pressed
+
+                // function is called when button is pressed
 function generatePassword() {
     let passwordPool = [];
-        // Prompt user to enter the password length and change the string to number
-    let userInput = parseInt(prompt("Please enter password length (between 8 and 128 characters)", "8"));
+                // Prompt user to enter the password length and change the string to number
+    let userInput = parseInt(prompt("Please enter password length (between 8 and 128 characters)", "16"));
 
-
-        // Check to see if password length is between 8 to 128 characters. If not, then exit function.
+                // Check to see if password length is between 8 to 128 characters. If not, then exit function.
     if (userInput >= 8 && userInput <= 128) {
-                // concat special characters to passwordPool array
+                // concat special characters to passwordPool passwordPool
          if (confirm("Do you want to include special characters?")) {
                 passwordPool = passwordPool.concat(specialCharacters);
-        } 
-                // concat numbers to passwordPool array
-        if (confirm("Do you want to include numbers")) {
+         
+                // concat numbers to passwordPool passwordPool
+        } if (confirm("Do you want to include numbers")) {
                 passwordPool = passwordPool.concat(numericCharacters);
-        } 
-            // concat lowercase characters to passwordPool array
-        if (confirm("Do you want to include lowercase letters?")) {
+         
+            // concat lowercase characters to passwordPool passwordPool
+        } if (confirm("Do you want to include lowercase letters?")) {
                 passwordPool = passwordPool.concat(lowerCasedCharacters);
         } 
-            // concat uppercase characters to passwordPool array
+            // concat uppercase characters to passwordPool passwordPool
         if (confirm("Do you want to include uppercase letters?")) {
                 passwordPool = passwordPool.concat(upperCasedCharacters)
         }
-        
+            // check if user selected at least 1 character type
         if (passwordPool.length === 0) {
-            alert ("Please select at least 1 type of characters");
+            alert ("Please select at least 1 character type.");
             return generatePassword();
         }
-
-    } else {
-            alert("Please enter a number between 8 and 128");
-            return
+            // prompt user to restart or cancel if they input an invalid response
+    } else if (confirm("Please enter a number between 8 and 128. Hit OK to restart or Cancel to quit")) {
+            return generatePassword();
         }
+        
+
 
     let passwordString = "";
     for (let i = 0; i<userInput; i++) {
