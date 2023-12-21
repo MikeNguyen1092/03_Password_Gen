8ab50256e8000acf2ba0ee9b5d5dc4bd39f14dbf
Mike Nguyen <mikenguyen@Mikes-Air.lan>
2023-12-20 22:13:37 -0500

    rm ; from if statements and it worked better. still selecting special character when I don't want it. rm , at at end of characters array

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 let generateBtn = document.querySelector(`#generate`);
 
 // Special characters 
-let specialCharacters = [`@`,`%`,`+`,`\\`,`/`,`'`,`!`,`#`,`$`,`^`,`?`,`:`,`,`,`)`,`(`,`}`,`{`,`]`,`[`,`~`,`-`,`_`,`.`,];
+let specialCharacters = [`@`,`%`,`+`,`\\`,`/`,`'`,`!`,`#`,`$`,`^`,`?`,`:`,`,`,`)`,`(`,`}`,`{`,`]`,`[`,`~`,`-`,`_`,`.`];
 
 // Numeric characters 
 let numericCharacters = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
 
 // Lowercase characters 
-let lowerCasedCharacters = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`,];
+let lowerCasedCharacters = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
 
 // Uppercase characters 
-let upperCasedCharacters = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`,];
+let upperCasedCharacters = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
 
 //Write password to the #password input on the HTML
 function writePassword() {
@@ -30,20 +30,20 @@ function generatePassword() {
         // Make userInput from string to a number
     let number = parseInt(userInput);
 
-        // Check to see if password length is between 8 to 128 characters. If not, then exit function. 
+        // TODO: Check to see if password length is between 8 to 128 characters. If not, then exit function. not working properly
     if (userInput >= 8 && userInput <= 128) {
         
          if (confirm("Do you want to include special characters?")); {
-                passwordPool = passwordPool.concat(specialCharacters);
+                passwordPool = passwordPool.concat(specialCharacters)
 
-        } if (confirm("Do you want to include numbers")); {
-                passwordPool = passwordPool.concat(numericCharacters);
+        } if (confirm("Do you want to include numbers")) {
+                passwordPool = passwordPool.concat(numericCharacters)
 
-        } if (confirm("Do you want to include lower cased characters?")); {
-                passwordPool = passwordPool.concat(lowerCasedCharacters);
+        } if (confirm("Do you want to include lower cased characters?")) {
+                passwordPool = passwordPool.concat(lowerCasedCharacters)
 
-        } if (confirm("Do you want to include upper cased characters?")); {
-                passwordPool = passwordPool.concat(upperCasedCharacters);
+        } if (confirm("Do you want to include upper cased characters?")) {
+                passwordPool = passwordPool.concat(upperCasedCharacters)
                 confirm(passwordPool.length)
      
         } if (passwordPool.length === 0) {
