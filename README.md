950401e58587ddaccc7e8ec1e8ce656b73b1bab6
Mike Nguyen <mikenguyen@Mikes-Air.lan>
2023-12-21 20:25:15 -0500

    Adding to README file

--- README.md
+++ README.md
@@ -1,25 +1,48 @@
 # 03 Password Generator
+## Description
+    I wanted a secure password that wouldn't be easily remembered. To achieve that, I built a JavaScript password generator. It let the user decide on the length of the password (between 8-128), and prompt the user to weather include numbers, lowercase, uppercase, or special characters. The user has pick a number between 8-128, and to include at lease one type of characters. If they don't, then there is a prompt to notify them, and the password won't be generated. 
 
-## Sources
+## Installation
+    Open the index.html in an internet browser
 
-MDN Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+## Usage
+    Open the index.html file in an internet browser. Click on the generate password button (image 1). You are then prompted to enter your desired password length between 8-128 characters, with 16 characters being the default (image 2). If the password length is less than 8 or more than 128 then a popup will alert the user (image 3). Next is if the user wants to include numbers, lowercase, uppercase, or special characters in the password. At lease one type of character has to be pick, or an alert will pop up saying "At least 1 type of character has to be pick" (image 4). The password will generate when all the criteria are met (image 5).
+![Image 1: A red button that reads Generate Password.](./assets/images/Generate_Password.png)
+![Image 2: A popup that asks you to enter your password length between 8-128 characters.](./assets/images/Password_length.png)
+![Image 3: A popup telling the user to pick a number between 8-128.](./assets/images/Number_8-128_restart.png)
+![Image 4: A popup that alerts the user that they need to pick at least one type of character](./assets/images/1_character_type.png)
+![Image 5: A password is generated with random numbers, letters and special characters.](./assets/images/Password_Generated.png)
 
-MDN JS Functions: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+## Credits
+    Michael Nguyen
 
-MDN parseInt(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+    Use code from this site to shuffle the array. Fisher-Yates algorithm: https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=The%20first%20and%20simplest%20way
 
-Window: confirm(): https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
+    EdX AI chat to help figure out some problems with code such as deleting semi-colons `;`.
 
-MDN Arrays: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+## Additional Sources
 
-MDN addEventListener(): https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+- MDN parseInt(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 
-MDN Array Methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Instance_methods
+- Window: confirm(): https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
 
-MDN Math.random(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+- MDN Arrays: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-MDN querySelector(): https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+- MDN Array Methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Instance_methods
 
-MDN concat(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+- MDN Math.random(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-used EdX AI chat to help
+- MDN querySelector(): https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+
+- MDN concat(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+## License
+    MIT License
+
+    Copyright (c) [2023] [Michael Nguyen]
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all  copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- script.js
+++ script.js
@@ -1,89 +1,177 @@
-                // Assignment Code, querySelector is used to select id=`generate` in HTML and assign it to generateBtn
+// Assignment Code, querySelector is used to select id=`generate` in HTML and assign it to generateBtn
 let generateBtn = document.querySelector(`#generate`);
 
-                // Global variables 
-                // Special characters 
-let specialCharacters = [`@`,`%`,`+`,`\\`,`/`,`'`,`!`,`#`,`$`,`^`,`?`,`:`,`,`,`)`,`(`,`}`,`{`,`]`,`[`,`~`,`-`,`_`,`.`];
+// Global variables
+// Special characters
+let specialCharacters = [
+  `@`,
+  `%`,
+  `+`,
+  `\\`,
+  `/`,
+  `'`,
+  `!`,
+  `#`,
+  `$`,
+  `^`,
+  `?`,
+  `:`,
+  `,`,
+  `)`,
+  `(`,
+  `}`,
+  `{`,
+  `]`,
+  `[`,
+  `~`,
+  `-`,
+  `_`,
+  `.`,
+];
 
-                // Numeric characters 
+// Numeric characters
 let numericCharacters = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
 
-                // Lowercase characters 
-let lowerCasedCharacters = [`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`];
+// Lowercase characters
+let lowerCasedCharacters = [
+  `a`,
+  `b`,
+  `c`,
+  `d`,
+  `e`,
+  `f`,
+  `g`,
+  `h`,
+  `i`,
+  `j`,
+  `k`,
+  `l`,
+  `m`,
+  `n`,
+  `o`,
+  `p`,
+  `q`,
+  `r`,
+  `s`,
+  `t`,
+  `u`,
+  `v`,
+  `w`,
+  `x`,
+  `y`,
+  `z`,
+];
 
-                // Uppercase characters 
-let upperCasedCharacters = [`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`];
+// Uppercase characters
+let upperCasedCharacters = [
+  `A`,
+  `B`,
+  `C`,
+  `D`,
+  `E`,
+  `F`,
+  `G`,
+  `H`,
+  `I`,
+  `J`,
+  `K`,
+  `L`,
+  `M`,
+  `N`,
+  `O`,
+  `P`,
+  `Q`,
+  `R`,
+  `S`,
+  `T`,
+  `U`,
+  `V`,
+  `W`,
+  `X`,
+  `Y`,
+  `Z`,
+];
 
-                // This Function is used to write password to the #password input on the HTML file.
+// This Function is used to write password to the #password input on the HTML file.
 function writePassword() {
-                // Calls and receive generated password from generatePassword function.
+  // Calls and receive generated password from generatePassword function.
   let password = generatePassword();
-                // Target id password in HTML using querySelector
+  // Target id password in HTML using querySelector
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-                // function is called when button is pressed
+// function is called when button is pressed
 function generatePassword() {
-    let passwordPool = [];
-                // Prompt user to enter the password length and change the string to number
-    let userInput = parseInt(prompt("Please enter password length (between 8 and 128 characters)", "16"));
+  let passwordPool = [];
+  // Prompt user to enter the password length and change the string to number
+  let userInput = parseInt(
+    prompt("Please enter password length (between 8 and 128 characters)", "16")
+  );
 
-                // Check to see if password length is between 8 to 128 characters. If not, then exit function.
-    if (userInput >= 8 && userInput <= 128) {
-                // concat special characters to passwordPool passwordPool
-         if (confirm("Do you want to include special characters?")) {
-                passwordPool = passwordPool.concat(specialCharacters);
-         
-                // concat numbers to passwordPool passwordPool
-        } if (confirm("Do you want to include numbers")) {
-                passwordPool = passwordPool.concat(numericCharacters);
-         
-            // concat lowercase characters to passwordPool passwordPool
-        } if (confirm("Do you want to include lowercase letters?")) {
-                passwordPool = passwordPool.concat(lowerCasedCharacters);
-        } 
-            // concat uppercase characters to passwordPool passwordPool
-        if (confirm("Do you want to include uppercase letters?")) {
-                passwordPool = passwordPool.concat(upperCasedCharacters)
-        }
-            // check if user selected at least 1 character type
-        if (passwordPool.length === 0) {
-            alert ("Please select at least 1 character type.");
-            return generatePassword();
-        }
-            // prompt user to restart or cancel if they input an invalid response
-    } else if (confirm("Please enter a number between 8 and 128. Hit OK to restart or Cancel to quit")) {
-            return generatePassword();
-        }
-            // This function is used to shuffle the passwordPool array. source: Fisher-Yates algorithm https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=The%20first%20and%20simplest%20way           
-    function shufflePasswordPool() {
-            // for loop, i = 0, ends loop when i > passwordPool.length, add 1 to i each loop
-        for (let i = 0; i < passwordPool.length; i++ ) {
+  // Check to see if password length is between 8 to 128 characters. If not, then exit function.
+  if (userInput >= 8 && userInput <= 128) {
+    // concat special characters to passwordPool passwordPool
+    if (confirm("Do you want to include special characters?")) {
+      passwordPool = passwordPool.concat(specialCharacters);
 
-            // j = random index number 0 <= j < passwordPool.length
-            const j = Math.floor(Math.random() * passwordPool.length);
-
-            // this swap the position of passwordPool[i] and passwordPool[j]
-            const temp = passwordPool[i];
-            passwordPool[i] = passwordPool[j]
-            passwordPool[j] = temp;
-        }
-        return passwordPool
+      // concat numbers to passwordPool passwordPool
     }
-            // calls shufflePasswordPool function
-    let shuffledPool = shufflePasswordPool(passwordPool);
+    if (confirm("Do you want to include numbers")) {
+      passwordPool = passwordPool.concat(numericCharacters);
 
-            // generate the password. getting a random index number and putting that value into an empty string
-    let passwordString = "";
-    for (let i = 0; i<userInput; i++) {
-        let randomPassword = Math.floor(Math.random() * shuffledPool.length);
-        passwordString += shuffledPool[randomPassword];
+      // concat lowercase characters to passwordPool passwordPool
     }
-    // returns the generated password to the writePassword function
-    return passwordString
-    
+    if (confirm("Do you want to include lowercase letters?")) {
+      passwordPool = passwordPool.concat(lowerCasedCharacters);
+    }
+    // concat uppercase characters to passwordPool passwordPool
+    if (confirm("Do you want to include uppercase letters?")) {
+      passwordPool = passwordPool.concat(upperCasedCharacters);
+    }
+    // check if user selected at least 1 character type
+    if (passwordPool.length === 0) {
+      alert("Please select at least 1 character type.");
+      return generatePassword();
+    }
+    // prompt user to restart or cancel if they input an invalid response
+  } else if (
+    confirm(
+      "Please enter a number between 8 and 128. Hit OK to restart or Cancel to quit"
+    )
+  ) {
+    return generatePassword();
+  }
+  /*    
+        This function is used to shuffle the passwordPool array. 
+        Source: Fisher-Yates algorithm https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj#:~:text=The%20first%20and%20simplest%20way
+        Added this function because I noticed that it wasn't really selection special characters every often.
+        */
+  function shufflePasswordPool() {
+    // for loop, i = 0, ends loop when i > passwordPool.length, add 1 to i each loop
+    for (let i = 0; i < passwordPool.length; i++) {
+      // j = random index number 0 <= j < passwordPool.length
+      const j = Math.floor(Math.random() * passwordPool.length);
+
+      // this swap the position of passwordPool[i] and passwordPool[j]
+      const temp = passwordPool[i];
+      passwordPool[i] = passwordPool[j];
+      passwordPool[j] = temp;
+    }
+    return passwordPool;
+  }
+  // calls shufflePasswordPool function
+  let shuffledPool = shufflePasswordPool(passwordPool);
+
+  // generate the password. getting a random index number and putting that value into an empty string
+  let passwordString = "";
+  for (let i = 0; i < userInput; i++) {
+    let randomPassword = Math.floor(Math.random() * shuffledPool.length);
+    passwordString += shuffledPool[randomPassword];
+  }
+  // returns the generated password to the writePassword function
+  return passwordString;
 }
 
 // Add event listener to generate button. generateBtn is linked to the HTML id. Once it is clicked, it will call the function writePassword
